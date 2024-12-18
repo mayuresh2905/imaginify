e8022301de518e58dc374119baf10206db989b97
Mayuresh <mayureshprabhu29@gmail.com>
2024-12-18 15:30:44 +0530

    Fixed transfoemations/update

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -8,7 +8,8 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { getImageById } from "@/lib/actions/image.actions";
 import { auth } from "@clerk/nextjs/server";
 
-const Page = async ({ params: { id } }: SearchParamProps) => {
+const Page = async ({ params }: SearchParamProps) => {
+  const { id } = await params;
   const { userId } = await auth();
 
   if (!userId) redirect("/sign-in");
